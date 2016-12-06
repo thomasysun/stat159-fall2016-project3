2ee0c6d664b0b45fae20fbc59ae8ee8f40dae035
thomasysun <thomasysun@berkeley.edu>
2016-12-06 01:08:17 -0800

    modified shinyapp fixed names

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -1,7 +1,14 @@
 
+
 library(ggplot2)
 
 clean_2012 <- readRDS("../data/clean_2012.rds")
+
+colnames(clean_2012) <- c("College Name", "School Type", "Total Students", "Percentage of White", "Percentage of Black",
+                          "Percentage of Hispanic", "Percentage of Asian", "Percentage of American Indian",
+                          "Percentage of non-Residential Alien", "4-year Completion Rate", "10-year Aggregate Earnings",
+                          "Students in Low Income Family", "Students in Mid Income Family", "Students with Federal Loans",
+                          "First Generation Students", "Cost of Attendance", "Average SAT Score")
 
 ui <- fluidPage(
   titlePanel("School Data"),
@@ -12,9 +19,9 @@ ui <- fluidPage(
            selectInput("man",
                        "School Name:",
                        c("All",
-                         unique(as.character(clean_2012$CollegeName))))
+                         unique(as.character(clean_2012$"College Name"))))
     )
-
+    
   ),
   # Create a new row for the table.
   fluidRow(
@@ -29,7 +36,7 @@ server <- function(input, output) {
   output$table <- DT::renderDataTable(DT::datatable({
     data <- clean_2012
     if (input$man != "All") {
-      data <- data[data$CollegeName == input$man,]
+      data <- data[data$'College Name' == input$man,]
     }
     data
   }))
